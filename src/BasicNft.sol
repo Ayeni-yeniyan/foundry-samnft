79d79e4bad2d888e2f8978d3d62e3551275c7ff5
Ayeni-yeniyan <connectwithayeni@gmail.com>
2025-03-29 08:15:29 +0100

    another commit

---
+++ src/BasicNft.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    constructor() ERC721("DoggieFt", "Dog") {}
+}
